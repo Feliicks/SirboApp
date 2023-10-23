7f120f252d140d89641a3ae36b7a56b76b2d9a0a
Felicks <thedarkomgf@gmail.com>
2023-10-23 13:42:26 -0400

    First commit

---
+++ app/src/main/java/com/felicksdev/onlymap/Home.kt
@@ -0,0 +1,120 @@
+package com.felicksdev.onlymap
+
+import android.annotation.SuppressLint
+import android.location.Location
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener
+import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Home.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Home : Fragment(),
+    OnMyLocationButtonClickListener,
+    OnMyLocationClickListener,
+    OnMapReadyCallback
+{
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private lateinit var mMap: GoogleMap
+    private var mapFragment: SupportMapFragment? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment?.getMapAsync(this)
+        return view
+
+    }
+
+    private fun createMarker() {
+
+        val favoritePlace= LatLng(
+            -34.0, 151.0
+        )
+        mMap.addMarker(
+            MarkerOptions().position(favoritePlace).title("Mi favorite palce")
+        )
+        mMap.animateCamera(
+            CameraUpdateFactory.newLatLng(favoritePlace),
+            4000,
+            null
+        )
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment Home.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            Home().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(googleMap: GoogleMap) {
+        Toast.makeText(context,"Soy el texto",Toast.LENGTH_LONG).show()
+        mMap=googleMap
+        createMarker()
+        mMap.isMyLocationEnabled = true
+        mMap.setOnMyLocationButtonClickListener(this)
+        mMap.setOnMyLocationClickListener(this)
+    }
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(context, "MyLocation button clicked", Toast.LENGTH_SHORT)
+            .show()
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false
+    }
+
+    override fun onMyLocationClick(location: Location) {
+        Toast.makeText(context, "Current location:\n$location", Toast.LENGTH_LONG)
+            .show()
+    }
+
+}
---
+++ app/src/main/java/com/felicksdev/onlymap/MainActivity2.kt
@@ -0,0 +1,90 @@
+package com.felicksdev.onlymap
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.felicksdev.onlymap.databinding.ActivityMain2Binding
+import com.felicksdev.onlymap.services.LocationService
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity2 : AppCompatActivity()  {
+    private lateinit var binding: ActivityMain2Binding
+    private lateinit var mapFragment: SupportMapFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        /*replaceFragment(Home())
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId){
+                R.id.home_fragment ->replaceFragment(Home())
+                R.id.addresses_fragment ->replaceFragment(Addresses())
+                R.id.routes_fragment ->replaceFragment(Routes())
+                else->{
+                }
+            }
+            true
+        }*/
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        //binding.bottomNavigationView.setupWithNavController(navController)
+        bottomNavigationView.setupWithNavController(navController)
+        //createFragment()
+        checkSelfPermission()
+        //mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        //mapFragment.getMapAsync(this) // Asegúrate de llamar a getMapAsync para activar onMapReady
+    }
+
+    private fun checkSelfPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+            requestLocationPermission()
+        }else{
+            //Abrir camara
+            Toast.makeText(this,"Permisos de ubicacion concedidos",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun requestLocationPermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_COARSE_LOCATION)){
+            //Usuario rechazó permisos
+            Toast.makeText(this,"Permisos de ubicacion rechazador por segunda vez",Toast.LENGTH_SHORT).show()
+        }else{
+            // pedir permisos
+            ActivityCompat.requestPermissions(this , arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),777)
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode==777){
+            if (grantResults.isNotEmpty()&& grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this,"Permisos de ubicacion concedidos",Toast.LENGTH_SHORT).show()
+            }else{
+                //
+                Toast.makeText(this,"Permisos rechazdo por primera vez",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+
+
+
+
+
+}
