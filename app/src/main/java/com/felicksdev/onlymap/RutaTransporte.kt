08ae4ac627ba75abf2e4208ccac1214d1ba45f05
Felicks <thedarkomgf@gmail.com>
2023-10-24 13:55:49 -0400

    Addes RutaTransporte Class

---
+++ app/src/main/java/com/felicksdev/onlymap/RutaTransporte.kt
@@ -0,0 +1,17 @@
+package com.felicksdev.onlymap
+
+data class RutaTransporte(
+    val codigoRuta: String,
+    val ruta: String,
+    val tipoRuta: String,
+    val tipoVehiculo: String,
+    val sentido: String,
+    val recorrido: String,
+    val operabilidad: String,
+    val operador: String,
+    val entidadMatriz: String,
+    val aprobacion: String
+)
+data class GeometriaRuta(
+    val coordenadas: List<List<List<Double>>>
+)
